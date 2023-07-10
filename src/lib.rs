4b55a0d5df7404586ea0c8e56a01a5578e21d83a
Alex Miller <codex.nz@gmail.com>
2023-07-10 15:53:20 +1200

    refactor: clean up

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,3 @@
-// use serde::Deserialize;
 use swc_core::plugin::{plugin_transform, proxies::TransformPluginProgramMetadata};
 use swc_core::{
     common::DUMMY_SP,
@@ -8,21 +7,10 @@ use swc_core::{
         visit::{as_folder, FoldWith, VisitMut, VisitMutWith},
     },
 };
-use tracing::trace;
 
 pub struct TransformVisitor;
 
-// #[derive(Deserialize)]
-// #[serde(rename_all = "camelCase")]
-// struct ImportMetaEnvOptions {
-//     some_option: Option<bool>,
-// }
-
 impl VisitMut for TransformVisitor {
-    // Implement necessary visit_mut_* methods for actual custom transform.
-    // A comprehensive list of possible visitor methods can be found here:
-    // https://rustdoc.swc.rs/swc_ecma_visit/trait.VisitMut.html
-
     fn visit_mut_expr(&mut self, n: &mut Expr) {
         n.visit_mut_children_with(self);
 
@@ -32,84 +20,69 @@ impl VisitMut for TransformVisitor {
 
         let member = n.as_mut_member().unwrap();
         if is_visiting_import_meta_env(member).is_some() {
-            // member.obj = Box::new(Ident::new("kdy1".into(), member.span.clone()).into());
-            // let mut x = Expr::Member(MemberExpr {
-            //     span: member.span.clone(),
-            //     obj: Box::new(Ident::new("process.env".into(), DUMMY_SP).into()),
-            //     prop: member.prop.clone(),
-            // });
-
-            // dbg!(x);
-
-            // n = x;
-            trace!("is_visiting_import_meta_env");
-            member.obj = Box::new(Ident::new("process".into(), DUMMY_SP).into());
-        } else {
-            trace!("ruh roah");
+            let obj = Box::new(Ident::new("process".into(), DUMMY_SP).into());
+            dbg!(&obj);
+            member.obj = obj;
         }
     }
 }
 
-fn is_visiting_import_meta_env(n: &MemberExpr) -> Option<()> {
+fn is_visiting_import_meta_env(n: &MemberExpr) -> Option<bool> {
     let obj = n.obj.as_meta_prop()?;
     let prop = n.prop.as_ident()?;
 
-    trace!("obj");
-    dbg!(&obj);
-    trace!("prop");
-    dbg!(&prop);
-
     if obj.kind != MetaPropKind::ImportMeta {
-        return None;
+        return Some(false);
     }
 
     if prop.sym.to_string() != "env" {
-        return None;
+        return Some(false);
     }
 
-    Some(())
+    Some(true)
 }
 
-/// An example plugin function with macro support.
-/// `plugin_transform` macro interop pointers into deserialized structs, as well
-/// as returning ptr back to host.
-///
-/// It is possible to opt out from macro by writing transform fn manually
-/// if plugin need to handle low-level ptr directly via
-/// `__transform_plugin_process_impl(
-///     ast_ptr: *const u8, ast_ptr_len: i32,
-///     unresolved_mark: u32, should_enable_comments_proxy: i32) ->
-///     i32 /*  0 for success, fail otherwise.
-///             Note this is only for internal pointer interop result,
-///             not actual transform result */`
-///
-/// This requires manual handling of serialization / deserialization from ptrs.
-/// Refer swc_plugin_macro to see how does it work internally.
 #[plugin_transform]
 pub fn process_transform(program: Program, _metadata: TransformPluginProgramMetadata) -> Program {
     program.fold_with(&mut as_folder(TransformVisitor))
 }
 
-// An example to test plugin transform.
-// Recommended strategy to test plugin's transform is verify
-// the Visitor's behavior, instead of trying to run `process_transform` with mocks
-// unless explicitly required to do so.
-// test!(
-//     Default::default(),
-//     |_| as_folder(TransformVisitor),
-//     og,
-//     // Input codes
-//     r#"console.log("transform");"#,
-//     // Output codes after transformed with plugin
-//     r#"console.log("transform");"#
-// );
+test!(
+    Default::default(),
+    |_| as_folder(TransformVisitor),
+    transform_import_meta_env,
+    r#"import.meta.env"#, // in
+    r#"process.env"#      // out
+);
 
 test!(
     Default::default(),
     |_| as_folder(TransformVisitor),
-    import_meta,
-    // Input codes
-    r#"import.meta.env"#,
-    // Output codes after transformed with plugin
-    r#"process.env"#
+    transform_import_meta_env_prop,
+    r#"import.meta.env.MODE"#, // in
+    r#"process.env.MODE"#      // out
+);
+
+test!(
+    Default::default(),
+    |_| as_folder(TransformVisitor),
+    transform_import_meta_env_key,
+    r#"import.meta.env["PROP"]"#, // in
+    r#"process.env["PROP"]"#      // out
+);
+
+test!(
+    Default::default(),
+    |_| as_folder(TransformVisitor),
+    no_transform_import_meta,
+    r#"import.meta"#, // in
+    r#"import.meta"#  // out
+);
+
+test!(
+    Default::default(),
+    |_| as_folder(TransformVisitor),
+    no_transform_import_meta_glob,
+    r#"import.meta.glob"#, // in
+    r#"import.meta.glob"#  // out
 );
