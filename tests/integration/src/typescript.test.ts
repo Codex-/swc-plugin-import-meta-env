6f3b6bba2b94e0a7ea8ede7169d7846730ee5e34
Alex Miller <codex.nz@gmail.com>
2024-01-23 13:42:18 +1300

    fix: type issues in IDE

--- tests/integration/src/typescript.test.ts
+++ tests/integration/src/typescript.test.ts
@@ -1,3 +1,7 @@
+interface ImportMeta {
+  readonly env: Record<string, any>;
+}
+
 describe("typescript", () => {
   it("should make import.meta.env use process.env and expose the object", () => {
     expect(typeof import.meta.env).toStrictEqual("object");
